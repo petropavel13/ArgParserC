edb1d8ab9ee6edc866d89dd797efbbe2f8dadc90
petropavel13 <mail4work0xFF@gmail.com>
2014-01-18 17:47:04 +0600

    completely redesigned; handling any prefix type

--- args.c
+++ args.c
@@ -2,58 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-int indexOf(char* src, char key) {
-  char* c = strchr(src, key);
-  return c == NULL ? -1 : c - src;
-}
+char* get_arg(const int argc, char** argv, const char* const key) {
+    int i = 0;
 
-char *substrFromIndex(char *src, int index) {
-  char *str = (char *)malloc(strlen(src) - index);
-  return strcpy(str, &(src[index]));
-}
-
-char *substrUntil(char *src, int index) {
-  char *str = (char *)malloc(index);
-  return strncpy(str, src, index);
-}
-
-char *getArg(int argc, char** argv, char* key) {
-
-  int i = 0;
-  for(; i < argc; i++) {
-    int index = indexOf(argv[i], '=');
-    char *strToComp = (index == -1) ? argv[i] : substrUntil(argv[i], index);
-    if(strcmp(strToComp, key) == 0) {
-      if(index != -1) {
-	return substrFromIndex(argv[i], index+1);
-      } else {
-	if(argv[i+1] && indexOf(argv[i+1], '-') != 0) {
-	  return argv[i+1];
-	} else {
-	  return "true";
-	}
-      }
+    for(; i < argc; ++i) {
+        if (strstr(argv[i], key) != NULL) {
+            if (strchr(argv[i], '=') != NULL) {
+                return &(argv[i][strlen(key) + 1]);
+            } else {
+                return "true";
+            }
+        }
     }
-  }
-  return "false";
+
+    return "false";
 }
 
 // --------------------------------------------------------------------
 //
-// ./a.out --test=my_arg -value
-//              ||         ||
-//              \/         \/
-//             my_arg     true
+// ./myapp --test=my_arg -value +abc=13 **silent
+//                ||        ||     ||      ||
+//                \/        \/     \/      \/
+//             "my_arg"   "true"  "13"   "true"
 //
 // I choose to return "true" and "false" rather than void pointers that would
-// need a cast. 
+// need a cast.
 // It's better to deal with string only, I think.
 // -------------------------------------------------------------------
 int main(int argc, char** argv) {
 
-  printf("%s\n", getArg(argc, argv, "--test")); // "my_arg"
-  printf("%s\n", getArg(argc, argv, "-value")); // "true"
-  printf("%s\n", getArg(argc, argv, "-opt"));   // "false"
-  
+  printf("%s\n", get_arg(argc, argv, "--test")); // "my_arg"
+  printf("%s\n", get_arg(argc, argv, "-value")); // "true"
+  printf("%s\n", get_arg(argc, argv, "-opt"));   // "false"
+  printf("%s\n", get_arg(argc, argv, "+abc"));   // "13"
+  printf("%s\n", get_arg(argc, argv, "**silent"));   // "true"
+
   return 42; // The answer of everything
 }
